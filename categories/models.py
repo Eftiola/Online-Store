968f558042cce4bc90351d15228bf1aed360a3ea
Eftiola Calo <eftiolacalo18@gmail.com>
2022-09-11 11:54:09 +0200

    initial commit

---
+++ categories/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self',related_name='children',on_delete=models.CASCADE,blank=True, null=True)
+    name = models.CharField(max_length=30, null=False, unique=True)
+    slug = models.SlugField(max_length=255,unique=True)
+    # is_active = models.BooleanField(default=True)
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return '/%s/' % (self.slug)    
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     return super().save(*args, **kwargs)
---
+++ categories/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+
+
+
+def get_categories(request):
+  return render(request, 'categories_list.html')
