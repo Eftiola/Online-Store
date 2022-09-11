bc2e08ec0016254e89c8c85f70151f877fb690b6
Eftiola Calo <eftiolacalo18@gmail.com>
2022-09-11 12:27:38 +0200

    Reorg project

--- categories/models.py
+++ categories/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
-    parent = models.ForeignKey('self',related_name='children',on_delete=models.CASCADE,blank=True, null=True)
+    parent = models.ForeignKey(
+        "self", related_name="children", on_delete=models.CASCADE, blank=True, null=True
+    )
     name = models.CharField(max_length=30, null=False, unique=True)
-    slug = models.SlugField(max_length=255,unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
     # is_active = models.BooleanField(default=True)
+
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "Categories"
@@ -14,7 +17,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return '/%s/' % (self.slug)    
+        return "/%s/" % (self.slug)
 
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.name)
