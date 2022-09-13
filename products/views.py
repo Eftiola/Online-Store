a6e488621afe16c7d836e3a7e35a8c659d8e55c1
Alketa Tari <alketa.tari7@gmail.com>
2022-09-13 11:23:23 +0200

    Add product form, 'product_add.html and edit product buton

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from categories.models import Category
 from .models import Product
 from django.core.paginator import Paginator
+from .forms import ProductForm
 
 
 
@@ -26,3 +27,32 @@ def get_products(request):
 def get_product_details(request, pk):
     product = get_object_or_404(Product,pk=pk)
     return render(request,"product_details.html",context={"product": product})
+
+
+def product_add(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.user = request.user
+            product.save()
+            return redirect("product_list")
+    else:
+        form = ProductForm()
+    return render(request, "product_add.html", context={
+        "form": form
+    })
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST,request.FILES, instance=product)
+        if form.is_valid():
+            product=form.save()
+            return redirect("product_list")
+    else:
+        form = ProductForm(instance=product)
+    return render(request, "product_add.html", context={
+        "form": form,
+        "product": product,
+    })
