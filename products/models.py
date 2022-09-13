d6b1d9b7394b468ca10bc3a51deac20805ec2a27
Alketa Tari <alketa.tari7@gmail.com>
2022-09-13 17:37:51 +0200

    Add orderline model

--- products/models.py
+++ products/models.py
@@ -29,3 +29,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class OrderLine(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    product_price=models.FloatField()
+    ordered = models.BooleanField(default=False)
+
+  
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.title}"
+    
+    def get_total_product_price(self):
+        return self.quantity * self.product.price
